b11f3cd8fc9546296f80a762ae83dfcec2b09277
Iradukundaalcene <iradalcene@gmail.com>
2022-01-18 21:38:51 +0200

    Ajout de chart-data.js dans Assets/js

--- GestionPanneGarageMvcWeb/Assets/js/chart-data.js
+++ GestionPanneGarageMvcWeb/Assets/js/chart-data.js
@@ -1,4 +1,5 @@
-var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
+
+var randomScalingFactor = function () { return Math.round(Math.random() * 1000) };
 	
 	var lineChartData = {
 		labels : ["January","February","March","April","May","June","July"],
