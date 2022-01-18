5497ace4672c586f845f77821bc04d1e216ed0ba
Iradukundaalcene <iradalcene@gmail.com>
2022-01-18 22:23:15 +0200

    Ajout de la fonction d'affichage suivant l'Id pour les autres utilisateurs nomné Details1

--- GestionPanneGarageMvcWeb/Controllers/ArticlesController.cs
+++ GestionPanneGarageMvcWeb/Controllers/ArticlesController.cs
@@ -12,11 +12,6 @@ namespace GestionPanneGarageMvcWeb.Controllers
     public class ArticlesController : Controller
     {
         private GestionPanneGarageEntities db = new GestionPanneGarageEntities();
-       
-        
-        
-
-
         //
         // GET: /Articles/
 
