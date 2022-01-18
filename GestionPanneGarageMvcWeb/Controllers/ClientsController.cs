8023140afefc4de25862e017cda3aceb3c5dc788
Iradukundaalcene <iradalcene@gmail.com>
2022-01-18 23:10:36 +0200

    Ajout de la fonction d'affichage suivant l'Id  pour les autres utilisateurs nomné(Details1)

--- GestionPanneGarageMvcWeb/Controllers/ClientsController.cs
+++ GestionPanneGarageMvcWeb/Controllers/ClientsController.cs
@@ -12,8 +12,6 @@ namespace GestionPanneGarageMvcWeb.Controllers
     public class ClientsController : Controller
     {
         private GestionPanneGarageEntities db = new GestionPanneGarageEntities();
-
-
         //
         // GET: /Clients/
 
